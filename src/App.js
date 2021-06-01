b6e8bd22a297d951bc7465b01d01bc835c4eaa73
Andrei <anton.andreii@example.com>
2021-06-01 15:23:41 +0300

    button toggle

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -46,10 +47,10 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask 
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask 
         onAddTask={addTask}
-      />
+      />}
       {tasks.length > 0 ? <Tasks 
         tasks={tasks}
         onDelete={deleteTask}
