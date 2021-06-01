b0738eadb10f113a2c7a014a6c600b8add417c60
Andrei <anton.andreii@example.com>
2021-06-01 15:16:37 +0300

    add new task to array

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -25,6 +26,13 @@ const App = () => {
     }
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -39,6 +47,9 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <AddTask 
+        onAddTask={addTask}
+      />
       {tasks.length > 0 ? <Tasks 
         tasks={tasks}
         onDelete={deleteTask}
